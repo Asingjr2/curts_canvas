5f1c5b7e1461bd1472510f6831497b867d71f924
asingjr2 <asingjr2@gmail.com>
2018-06-24 23:10:25 -0400

    added some rating code.  Need to correct layout, pull existing ratings and display.  Can then add twitter information

--- curts_canvas/canvas/admin.py
+++ curts_canvas/canvas/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from .models import Picture
+from .models import Picture, Rating
 
 
 @admin.register(Picture)
 class PictureAdmin(admin.ModelAdmin):
-    fields = ("name", "creator", "description", "price")
+    fields = ("name", "creator", "description", "price", "avg_rating", "total_rating")
     list_display = ["name"]
 
 
+admin.site.register(Rating)
